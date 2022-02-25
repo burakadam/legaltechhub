54c6a1d0373bc92cf3b91eaa0a69276ec73fd3e4
Burak Erden <burakerden@Buraks-MacBook-Pro.local>
2022-02-25 09:52:53 +0300

    open-close menu

--- assets/inc/functions.js
+++ assets/inc/functions.js
@@ -1,78 +1,79 @@
 function init() {
-  clicker("#login", () => {
-    window.location.href = "/main.html";
+  clicker('#login', () => {
+    window.location.href = '/main.html';
   });
 
-  clicker("#header__menu-btn", (e) => {
-    document.getElementById("menu").classList.toggle("active");
+  clicker('#header__menu-btn', (e) => {
+    document.getElementById('menu').classList.toggle('active');
+    document.body.classList.toggle('menu-opened');
   });
 
-  clicker(".header__container-btn", (e) => {
-    var id = e.getAttribute("data-id");
+  clicker('.header__container-btn', (e) => {
+    var id = e.getAttribute('data-id');
     var el = document.getElementById(id);
 
-    if (hasClass(el, "active")) {
-      removeClass(el, "active");
-      removeClass(e, "active");
+    if (hasClass(el, 'active')) {
+      removeClass(el, 'active');
+      removeClass(e, 'active');
     } else {
       removeClass(
-        "header .blue-container,#header___buttons > button",
-        "active"
+        'header .blue-container,#header___buttons > button',
+        'active'
       );
 
-      addClass(el, "active");
-      addClass(e, "active");
+      addClass(el, 'active');
+      addClass(e, 'active');
     }
   });
 
-  clicker(".close-btn", (e) => {
-    e.parentNode.classList.toggle("active");
+  clicker('.close-btn', (e) => {
+    e.parentNode.classList.toggle('active');
   });
 
-  clicker(".tab", (e) => {
-    removeClass(e.parentNode.children, "active");
-    addClass(e, "active");
+  clicker('.tab', (e) => {
+    removeClass(e.parentNode.children, 'active');
+    addClass(e, 'active');
   });
 
-  clicker(".tabs.dropdown .tab", (e) => {
+  clicker('.tabs.dropdown .tab', (e) => {
     resetItems(e);
   });
 
-  clicker(".document-box > button", (e) => {
-    e.parentNode.classList.toggle("opened");
+  clicker('.document-box > button', (e) => {
+    e.parentNode.classList.toggle('opened');
 
-    e.parentNode.querySelectorAll("iframe")[0].style.height = hasClass(
+    e.parentNode.querySelectorAll('iframe')[0].style.height = hasClass(
       e.parentNode,
-      "opened"
+      'opened'
     )
-      ? "693px"
+      ? '693px'
       : 0;
     0;
   });
 
-  clicker(".tabs.dropdown", (e) => {
-    e.classList.toggle("opened");
+  clicker('.tabs.dropdown', (e) => {
+    e.classList.toggle('opened');
   });
 
-  clicker("#template-reject", () => {
-    addClass("#overlay", "active");
+  clicker('#template-reject', () => {
+    addClass('#overlay', 'active');
   });
 
-  clicker("#overlay .close-btn", () => {
-    removeClass("#overlay", "active");
+  clicker('#overlay .close-btn', () => {
+    removeClass('#overlay', 'active');
   });
 }
 
 function resetItems(e) {
-  removeClass(e.parentNode.children, "active");
+  removeClass(e.parentNode.children, 'active');
   var cln = e.cloneNode(true);
   var parent = e.parentNode;
-  cln.addEventListener("click", function (e) {
+  cln.addEventListener('click', function (e) {
     resetItems(this);
   });
 
-  cln.classList = "tab active";
+  cln.classList = 'tab active';
 
   e.remove();
-  parent.insertBefore(cln, parent.querySelectorAll("button")[0]);
+  parent.insertBefore(cln, parent.querySelectorAll('button')[0]);
 }
